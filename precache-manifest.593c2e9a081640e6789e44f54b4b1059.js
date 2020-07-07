b01893e85c97d3ed5c88b815b5f3c2682f44ef7a
Spider King <gollyzoom@gmail.com>
2020-07-07 12:51:10 -0700

    Updates

---
+++ precache-manifest.593c2e9a081640e6789e44f54b4b1059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975151bdc466b9ef8445c781a15b953b",
+    "url": "/L-C-Work/index.html"
+  },
+  {
+    "revision": "5885c4cc4dcd0019dd99",
+    "url": "/L-C-Work/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4a5c3059ae9bdd796315",
+    "url": "/L-C-Work/static/js/2.2b369069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/L-C-Work/static/js/2.2b369069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5885c4cc4dcd0019dd99",
+    "url": "/L-C-Work/static/js/main.ed826e5f.chunk.js"
+  },
+  {
+    "revision": "df9c814df84641d2acab",
+    "url": "/L-C-Work/static/js/runtime-main.9ccd1536.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/L-C-Work/static/media/logo.5d5d9eef.svg"
+  }
+]);
